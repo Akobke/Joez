e2aa7488dbad4255ba90de77a71c195965d49e6e
shotspike3 <53101884+shotspike3@users.noreply.github.com>
2021-09-25 22:42:20 -0400

    Add files via upload
    the joeing may be complete

--- src/index.js
+++ src/index.js
@@ -3,6 +3,39 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {isWeekend,getDayName} from './dates';
+
+
+const calendar = document.querySelector("#app-calendar");
+
+
+for (let day = 1; day <= 31; day++){
+  console.log(day)
+
+  const weekend = isWeekend(day);
+
+  let name = "";
+
+  if(day <= 7){
+    const dayName = getDayName(day);
+    name = `<div class="name">${dayName}</div>`;
+  }
+  const dayName = getDayName(day);
+
+  calendar.insertAdjacentHTML("beforeend", `<div 
+  class="day ${weekend ? "weekend" : ""}"><div class="name">${name}</div>${day}</div>`);
+}
+
+document.querySelectorAll("#app-calendar .day").forEach(
+  day => {
+    day.addEventListener("click", event => {
+      event.currentTarget.classList.toggle("selected");
+    })
+  }
+)
+
+
+
 
 ReactDOM.render(
   <React.StrictMode>
